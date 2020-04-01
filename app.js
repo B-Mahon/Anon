3483602dec6db37676e08bba24ec749d675239f0
Blaine McMahon <bmcmahon@ad.checkpoint.com>
2020-04-01 14:51:49 -0400

    Security Level 1

---
+++ app.js
@@ -0,0 +1,86 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/anonDB", {
+  useNewUrlParser: true
+});
+
+
+//Create Schema
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+//GET Method
+//Send user back signup page when they go to HOME route
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+//GET Method
+//Send user back signup page when they go to HOME route
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+//GET Method
+//Send user back signup page when they go to HOME route
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+app.post("/signup", function(req, res) {
+  console.log(req.body.email);
+  console.log(req.body.pass);
+
+  //Add user to database
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.pass
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("page");
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.email;
+  const password = req.body.pass;
+console.log("kk");
+  User.findOne({
+    email:username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log("user not found");
+    } else {
+      if (foundUser) {
+        if(foundUser.password === res.render("page"));
+      }
+    }
+  });
+});
+
+
+
+app.listen(3000, function() {
+  console.log("server is spinnin up");
+});

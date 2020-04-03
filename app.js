1620c75a0e61f81fd25f52e7c9d049a7170fbd67
Blaine McMahon <bmcmahon@ad.checkpoint.com>
2020-04-03 17:56:53 -0400

    Heroku google oauth fix

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/anon",
+    callbackURL: "https://sheltered-woodland-75457.herokuapp.com/auth/google/anon",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {

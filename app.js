a7613b08557cb13511f39c0bd073e8f83fe18b43
Blaine McMahon <bmcmahon@ad.checkpoint.com>
2020-04-07 19:50:09 -0400

    bug fixes

--- app.js
+++ app.js
@@ -177,11 +177,11 @@ User.update({'googleId':req.body.id},{$set:{"likes":req.body.likes}});
 });
 
 
-/*let port = process.env.PORT;
+let port = process.env.PORT;
 if(port == null || port == ""){
   port = 3000;
-}*/
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000.");
 });

a0ef7679e1289508bc324d69de33a64551b9e7d0
Blaine McMahon <bmcmahon@ad.checkpoint.com>
2020-04-07 20:43:26 -0400

    Like bug fix 2

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,13 +1,13 @@
 $(".fa-thumbs-up").click(function(e) {
   postId= e.target.getAttribute('name');
   var numLikes = parseInt($(this).text());
-console.log('wtf');
+
 //User Unlikes Post
   if ($(this).hasClass("thumbs-up")) {
     $(this).toggleClass("thumbs-up");
     $(this).text(function(i, oldValue) {
       $.ajax({
-              url: 'http://localhost:3000/like',
+              url: 'https://sheltered-woodland-75457.herokuapp.com/like',
              contentType: "application/json",
               data: JSON.stringify({id:postId,likes:parseInt(oldValue - 1)}),
               type: 'POST',
@@ -20,7 +20,7 @@ console.log('wtf');
     $(this).toggleClass("thumbs-up");
     $(this).text(function(i, oldValue) {
       $.ajax({
-              url: 'http://localhost:3000/like',
+              url:  'https://sheltered-woodland-75457.herokuapp.com/like',
              contentType: "application/json",
                 data: JSON.stringify({id:postId,likes:parseInt(oldValue + 1)}),
               type: 'POST',
